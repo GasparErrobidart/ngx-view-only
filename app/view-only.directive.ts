b758af0149220b6c39c341de792d7ba2b556db97
GasparErrobidart <podmaster@hotmail.com>
2018-06-08 14:52:58 -0300

    in progress directive

--- app/view-only.directive.ts
+++ app/view-only.directive.ts
@@ -81,7 +81,7 @@ export class ViewOnlyDirective {
   visibilityCheck(){
     this.DOMElements.forEach((el)=>{
       if(!this.isInView(el)){
-        
+
       }
     })
   }
@@ -98,7 +98,7 @@ export class ViewOnlyDirective {
     return {
       top:     el.getBoundingClientRect().top,
       right:   el.getBoundingClientRect().left + el.offsetWidth,
-      bottom:  el.getBoundingClientRect().top + el.offsetHeight,
+      bottom:  el.getBoundingClientRect().top  + el.offsetHeight,
       left:    el.getBoundingClientRect().left,
       height:  el.offsetHeight,
       width:   el.offsetWidth
